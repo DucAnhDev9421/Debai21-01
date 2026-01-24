7a349e8a355a4174566e5785e92bb75eee6c0b33
DucAnhDev9421 <ntducanh9421@gmail.com>
2026-01-24 19:17:28 +0700

    Tạo mảng mới

--- Product.js
+++ Product.js
@@ -8,11 +8,20 @@ function Product(id, name, price, quantity, category, isAvailable)
     this.isAvailable = isAvailable;
 }
 
-var product = [
+var product1 = [
     new Product(1, "Điện thoại Samsung", 4200000, 20, "Điện thoại", true),
     new Product(2, "Điện thoại iPhone 17", 14990000, 12, "Điện thoại", true),
     new Product(3, "Laptop Acer Nitro 5", 13500000, 8, "Laptop", true),
     new Product(4, "Laptop Lenovo ThinkPad", 18900000, 5, "Laptop", false),
     new Product(5, "Tai nghe Sony", 1290000, 30, "Thiết bị âm thanh", true),
     new Product(6, "Loa JBL", 990000, 18, "Thiết bị âm thanh", true)
+];
+
+var product2 = [
+    { name: "Điện thoại Samsung", price: 4200000 },
+    { name: "Điện thoại iPhone 17", price: 14990000 },
+    { name: "Laptop Acer Nitro 5", price: 13500000 },
+    { name: "Laptop Lenovo ThinkPad", price: 18900000 },
+    { name: "Tai nghe Sony", price: 1290000 },
+    { name: "Loa JBL", price: 990000 }
 ];
